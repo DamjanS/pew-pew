38e970e3c59b47275225229d761c090c93ee3c4a
damjan.spevec <damjan.spevec@fiveminutes.eu>
2017-05-19 10:38:35 +0200

    First push

---
+++ Assets/Scripts/StartFromFirstLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartFromFirstLevel : MonoBehaviour {
+
+	// respond on collisions
+	void OnCollisionEnter(Collision newCollision)
+	{
+		// only do stuff if hit by a projectile
+		if (newCollision.gameObject.tag == "Projectile") {
+			// call the Start from First level function in the game manager
+			GameManager.gm.StartGameFromFirstLevel();
+		}
+	}
+}
